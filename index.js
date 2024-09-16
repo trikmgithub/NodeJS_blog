0f8e970172fdf5c7598aea74d5416769f523521a
trikmgithub <trikmforwork@gmail.com>
2024-09-17 00:44:15 +0700

    second change

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 const port = 3000
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
     var a = 1;
