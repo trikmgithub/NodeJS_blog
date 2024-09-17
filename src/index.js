ccdc6fdf10ddedbef68ede109d5c3910dbe7e41d
trikmgithub <trikmforwork@gmail.com>
2024-09-17 21:28:44 +0700

    third commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+  const express = require('express');
+  const morgan = require('morgan');
+  const path = require('path');
+  const { engine : handlebars } = require('express-handlebars');
+  const app = express();
+  const port = 3000;
+
+  app.use(express.static(path.join(__dirname, 'public')));
+ 
+  // HTTP logger
+  app.use(morgan('combined'));
+  // Templates engine
+  app.engine('.hbs', handlebars({extname: '.hbs'}));
+  app.set('view engine', '.hbs');
+  app.set('views', path.join(__dirname, 'resources/views'));
+
+  app.use(express.static(path.join(__dirname, 'public')));
+  console.log("conmemay" + path.join(__dirname, 'public'));
+
+  app.get('/', (req, res) => {
+    res.render('home');
+  });
+
+  app.get('/news', (req, res) => {
+    res.render('news');
+  });
+
+  
+
+  try {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+    
+  } catch (error) {
+    window.log('https://google.com/?q=' + error)
+  }
